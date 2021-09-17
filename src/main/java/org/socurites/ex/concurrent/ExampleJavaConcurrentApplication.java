1813d70891b0ae3d3a1bdb9323c85e39e58f811d
aaronsleepy <aaron@kmong.com>
2021-09-17 13:02:21 +0900

    first commit

---
+++ src/main/java/org/socurites/ex/concurrent/ExampleJavaConcurrentApplication.java
@@ -0,0 +1,13 @@
+package org.socurites.ex.concurrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleJavaConcurrentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleJavaConcurrentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/socurites/ex/concurrent/ExampleJavaConcurrentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.socurites.ex.concurrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleJavaConcurrentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
